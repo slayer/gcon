94f4b7ff763d056a3192cb8856a070710e04c2a2
Vlad <devvlad@gmail.com>
2026-01-12 15:08:51 +0200

    2026-01-12: Show upload hint in empty bucket/folder view

--- internal/ui/views/objects.go
+++ internal/ui/views/objects.go
@@ -526,7 +526,7 @@ func (v *ObjectsView) View() string {
 		if v.currentPrefix != "" {
 			msg = "This folder is empty."
 		}
-		return fmt.Sprintf("\n  %s\n  Press 'esc' to go back.", msg)
+		return fmt.Sprintf("\n  %s\n  Press 'u' to upload files, 'esc' to go back.", msg)
 	}
 
 	// Build pagination info
