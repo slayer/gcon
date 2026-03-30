27e6603810c2efdab037daa80e54d1f5c7314031
Vlad <slayer@users.noreply.github.com>
2026-03-30 20:24:32 +0300

    2026-03-30: fix table widths for 126-col terminal (VHS at 1200px/14pt)

--- internal/ui/views/instances.go
+++ internal/ui/views/instances.go
@@ -115,11 +115,11 @@ func defaultInstanceKeyMap() instanceKeyMap {
 // Table column definitions
 func instanceColumns() []table.Column {
 	return []table.Column{
-		{Title: "Name", Width: 24, Grow: true, Sortable: true},
-		{Title: "Zone", Width: 18, Sortable: true},
-		{Title: "Internal IP", Width: 15},
-		{Title: "External IP", Width: 15},
-		{Title: "Machine Type", Width: 14, Sortable: true},
+		{Title: "Name", Width: 20, Grow: true, Sortable: true},
+		{Title: "Zone", Width: 16, Sortable: true},
+		{Title: "Internal IP", Width: 14},
+		{Title: "External IP", Width: 14},
+		{Title: "Machine Type", Width: 12, Sortable: true},
 	}
 }
 
